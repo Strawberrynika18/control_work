2bf0d32cd43f29155d92a6662179523cf0191251
Strawberrynika18 <113605432+Strawberrynika18@users.noreply.github.com>
2022-11-24 01:21:38 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Write("введите кол-во элементов в массиве ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string[] array = new string[n];
+
+string[] arrayresult = new string[n];
+int len = 3;
+int pos = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine($"Введите {i+1}-й элемент: ");
+    string element = Convert.ToString(Console.ReadLine());
+    array[i] = element;
+}
+
+
+for (int j = 0; j < array.Length; j++)
+{
+    if (array[j].Length <= len)
+    {
+        arrayresult [pos] = array[j];
+        pos++;
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+PrintArray(arrayresult);
